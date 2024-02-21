95ae898173f1d6cef626fd1133afe79e43a6cc81
eloisapsl <111752882+eloisapsl@users.noreply.github.com>
2024-02-21 08:42:44 -0300

    Add files via upload

---
+++ aula21-02-24/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula21_02_24_eloisa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta1 = new ContaCorrente("Eloisa", 5000);
+            conta1.Depositar(2000);
+            conta1.Depositar(1420);
+            conta1.Sacar(350);
+        }
+    }
+}
---
+++ aula21-02-24/aula21-02-24/ContaCorrente.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula21_02_24_eloisa
+{
+    internal class ContaCorrente
+    {
+        private string proprietario { get; set; }
+        private double saldo { get; set; }
+
+        public ContaCorrente(string proprietario, double saldo)
+        {
+            this.proprietario = proprietario;
+            this.saldo = saldo;
+        }
+
+        public Boolean Sacar(double valor_saque)
+        {
+            if (saldo >= valor_saque)
+            {
+                saldo = saldo - valor_saque;
+                Console.WriteLine($"Saque de R${valor_saque} realizado.");
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+            return true;
+        }
+
+        public Boolean Depositar(double valor_deposito)
+        {
+            saldo = saldo + valor_deposito;
+            Console.WriteLine($"Depósito de R${valor_deposito} realizado.");
+            return true;
+        }
+    }
+}
