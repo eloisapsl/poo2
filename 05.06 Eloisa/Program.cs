4ce935c124fd2c35b0a9cfba98779722346d522e
Eloisa Pajehú <111752882+eloisapsl@users.noreply.github.com>
2024-06-05 08:42:19 -0300

    Add files via upload

---
+++ 05.06 Eloisa/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace _05._06_Eloisa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection conexao;
+            conexao = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Banco00;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            try
+            {
+                conexao.Open();
+                Console.WriteLine("Conexão aberta!");
+
+                /* Parte 2 Inserir Dados
+                var insertCmd = conexao.CreateCommand();
+                insertCmd.CommandText = "INSERT INTO Exemplo(Nome, Sobrenome) VALUES(@nome, @sobrenome)";
+
+                var parmNome = new SqlParameter("nome", "Maria");
+                var parmSobrenome = new SqlParameter("sobrenome", "Silva");
+
+                insertCmd.Parameters.Add(parmNome);
+                insertCmd.Parameters.Add(parmSobrenome);
+
+                insertCmd.ExecuteNonQuery();
+                Console.WriteLine("Linha inserida!"); */
+
+                /* Parte 2 Inserir Dados */
+                var selectCmd = conexao.CreateCommand();
+                selectCmd.CommandText = "SELECT * FROM Exemplo";
+
+                SqlDataReader leitorDados = selectCmd.ExecuteReader();
+
+                while (leitorDados.Read())
+                {
+                    Console.WriteLine("Nome: " + leitorDados["Nome"] + "Sobrenome: " + leitorDados["Sobrenome"]);
+                }
+                }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Erro!");
+
+            }
+            finally
+            {
+                conexao.Close();
+                Console.WriteLine("Conexão fechada!");
+            }
+        }
+    }
+}
---
+++ 05.06 Eloisa/SQLQuery1.sql
@@ -0,0 +1,13 @@
+﻿
+CREATE TABLE [dbo].[Exemplo]
+(--chave primaria auto encrement
+[Id] INT IDENTITY (1,1) NOT NULL PRIMARY KEY,
+[Nome] NVARCHAR(MAX),
+[Sobrenome] NVARCHAR(MAX)
+);
+
+INSERT INTO Exemplo (Nome, Sobrenome) VALUES ('Cássio', 'Capucho');
+INSERT INTO Exemplo (Nome, Sobrenome) VALUES ('João', 'Silva');
+INSERT INTO Exemplo (Nome, Sobrenome) VALUES ('Pedro', 'Souza');
+
+SELECT * FROM Exemplo;
